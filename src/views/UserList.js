b285bb317371c1d8c444c3e66ba5405686758485
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-06-10 00:51:28 -0300

    first version of list that's all right

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import users from "../data/users";
+import { Accessory, Avatar, ListItem } from "react-native-elements";
+import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content";
 
 export default (props) => {
+  function getUserItem({ item: user }) {
+    return (
+      <>
+        <ListItem
+          bottomDivider
+          onPress={() => props.navigation.navigate("UserForm")}>
+          <Avatar source={{ uri: user.avatarUrl }}></Avatar>
+          <ListItemContent>
+            <ListItem.Title>{user.name}</ListItem.Title>
+            <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+          </ListItemContent>
+          <ListItem.Chevron />
+        </ListItem>
+      </>
+    );
+  }
+
   return (
-    <>
-      <Text>UserList</Text>
-    </>
+    <View>
+      <FlatList
+        keyExtractor={(user) => user.id}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   );
 };
